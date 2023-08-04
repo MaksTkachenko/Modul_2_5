2ec226d4b01f94c2cfae12d0bdaa94b687ee715d
MaksTkachenko <maksym_pro@ukr.net>
2023-08-05 00:51:10 +0300

    added task_4

---
+++ task_4.py
@@ -0,0 +1,10 @@
+# Напишіть генератор, який видає послідовність квадратів чисел від 1 до N.
+
+
+def square_generator(n):
+    for i in range(1, n + 1):
+        yield i ** 2
+
+
+for square in square_generator(5):
+    print(square)
