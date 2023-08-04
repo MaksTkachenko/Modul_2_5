6b3b05c2643ae7365110d2d1dda77951b7522c08
MaksTkachenko <maksym_pro@ukr.net>
2023-08-05 00:37:18 +0300

    added task_2

---
+++ task_2.py
@@ -0,0 +1,13 @@
+# Напишіть генератор, який повертає послідовність чисел Фібоначчі.
+
+
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+fib_gen = fibonacci_generator()
+for i in range(10):
+    print(next(fib_gen))
