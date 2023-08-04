d4891fca564daf71c435ae545fd32c66023595f1
MaksTkachenko <maksym_pro@ukr.net>
2023-08-05 02:08:18 +0300

    correction task_5

--- task_5.py
+++ task_5.py
@@ -1,12 +1,28 @@
 # Реалізуйте ітератор для перебору всіх ключів словника.
 
-def dict_keys_iterator(diction):
-    for i in diction.keys():
-        yield i
+class DictKeysIterator:
+
+    def __init__(self, diction):
+        self.diction = diction
+        self.index = 0
+        self.keys = list(dictionary.keys())
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        if self.index < len(self.diction.keys()):
+
+            key_2 = self.keys[self.index]
+            self.index += 1
+            return key_2
+        else:
+            raise StopIteration
 
 
 dictionary = {"a": 1, "b": 2, "c": 3}
-dict_iter = dict_keys_iterator(dictionary)
+dict_iter = DictKeysIterator(dictionary)
 
 for key in dict_iter:
     print(key)
