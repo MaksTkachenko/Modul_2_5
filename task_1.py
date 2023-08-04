59a9f0d5d9c8b756868f5f5055e4728cff44daa4
MaksTkachenko <maksym_pro@ukr.net>
2023-08-05 00:12:06 +0300

    added task_1

---
+++ task_1.py
@@ -0,0 +1,19 @@
+# Змініть функцію even_odd_generator так, щоб вона генерувала послідовність
+# чисел від 1 до n з рядками "Fizz" для чисел, які діляться на 3, "Buzz" для
+# чисел, які діляться на 5, і "FizzBuzz" для чисел, які діляться як на 3, так і на 5.
+
+
+def even_odd_generator(num):
+    for i in range(1, num + 1):
+        if i % 3 == 0:
+            yield "Fizz"
+        if i % 5 == 0:
+            yield "Buzz"
+        if i % 3 == 0 and i % 5 == 0:
+            yield "FizzBuzz"
+        else:
+            yield i
+
+
+for el in even_odd_generator(15):
+    print(el)
