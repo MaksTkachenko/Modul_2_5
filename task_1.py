41fb72c08f1f7b97f575baf95cf044e7b2e83800
MaksTkachenko <maksym_pro@ukr.net>
2023-08-07 21:18:00 +0300

    correction task_1

--- task_1.py
+++ task_1.py
@@ -5,12 +5,12 @@
 
 def even_odd_generator(num):
     for i in range(1, num + 1):
-        if i % 3 == 0:
-            yield "Fizz"
-        if i % 5 == 0:
-            yield "Buzz"
         if i % 3 == 0 and i % 5 == 0:
             yield "FizzBuzz"
+        elif i % 3 == 0:
+            yield "Fizz"
+        elif i % 3 == 0:
+            yield "Buzz"
         else:
             yield i
 
